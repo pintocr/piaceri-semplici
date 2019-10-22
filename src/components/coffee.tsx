947e0a7aba26227e9afd44cc0afabb2d6bbf86f1
trierdo <trierdo@mediamarktsaturn.com>
2019-10-22 22:09:44 +0200

    dynamic data for fader

--- src/components/coffee.tsx
+++ src/components/coffee.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import Axios from 'axios';
 import '../stylesheets/coffee.scss';
 
-const img1: string = process.env.REACT_APP_BACKEND + '/images/coffee1.jpg';
-const img2: string = process.env.REACT_APP_BACKEND + '/images/coffee2.jpg';
-const img3: string = process.env.REACT_APP_BACKEND + '/images/coffee3.jpg';
-const img4: string = process.env.REACT_APP_BACKEND + '/images/coffee4.jpg';
-
 interface ICategory {
     _id: String;
     name: String;
@@ -17,22 +12,34 @@ interface ICategory {
 interface IState {
     categoryId: String;
     categoryDescription: String;
- }
+    pictureList: string[];
+}
 interface IProps { }
 
 export default class Coffee extends React.PureComponent<IProps, IState> {
 
-    //  constructor(props: IProps) {
-    //      super(props);
+    constructor(props: IProps) {
+        super(props);
 
-    //  }
+        this.state = {
+            categoryId: '',
+            categoryDescription: '',
+            pictureList: []
+        }
+    }
 
     componentDidMount() {
 
         Axios.get(`${process.env.REACT_APP_BACKEND}/category`)
             .then(res => {
-                console.log(res.data.filter((cat: ICategory) => cat.name === 'Coffee'))
-
+                let categoryInfo = res.data.filter((cat: ICategory) => cat.name === 'Coffee')[0];
+                this.setState({
+                    categoryId: categoryInfo._id,
+                    categoryDescription: categoryInfo.description,
+                    pictureList: categoryInfo.pic_list.map((pic: String) =>
+                        process.env.REACT_APP_BACKEND + '/images/' + pic)
+                });
+                console.log(this.state)
             });
     }
 
@@ -43,12 +50,13 @@ export default class Coffee extends React.PureComponent<IProps, IState> {
             <div>
 
                 <div className="image-wrapper">
-                    <div className="images image1"><img src={img1} alt="pic"/></div>
-                    <div className="images image2"><img src={img2} alt="pic"/></div>
-                    <div className="images image3"><img src={img3} alt="pic"/></div>
-                    <div className="images image4"><img src={img4} alt="pic"/></div>
+                    <div className="description">{this.state.categoryDescription}</div>
+                    <div className="images image1"><img src={this.state.pictureList[0]} alt="pic" /></div>
+                    <div className="images image2"><img src={this.state.pictureList[1]} alt="pic" /></div>
+                    <div className="images image3"><img src={this.state.pictureList[2]} alt="pic" /></div>
+                    <div className="images image4"><img src={this.state.pictureList[3]} alt="pic" /></div>
                 </div>
-                
+
             </div>
         )
     }
