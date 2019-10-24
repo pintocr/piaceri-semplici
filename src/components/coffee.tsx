5423498fca43ad650a56892806445064c3345c73
trierdo <trierdo@mediamarktsaturn.com>
2019-10-24 15:20:47 +0200

    dynamic product list from db

--- src/components/coffee.tsx
+++ src/components/coffee.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import Axios from 'axios';
 import '../stylesheets/coffee.scss';
+import { Card, Col, Row } from 'antd';
+import { Mongoose } from 'mongoose';
+
+const cardStyle = {
+    width: 300,
+    overflow: 'hidden',
+    display: 'flex',
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 12,
+    flexGrow: 1,
+};
+
+const bodyStyle = {
+    width: 300,
+    justifyContent: "center",
+    alignItems: "center",
+}
 
 interface ICategory {
     _id: String;
@@ -9,10 +27,25 @@ interface ICategory {
     pic_list: String[];
 }
 
+interface IProduct {
+    _id: string;
+    title: string;
+    product_id: string;
+    description: string;
+    manufacturer: string;
+    price: number;
+    amount: number;
+    unit: string;
+    ref_category: string;
+    pic_list: string[];
+    rating: [];
+}
+
 interface IState {
     categoryId: String;
     categoryDescription: String;
     pictureList: string[];
+    products: [];
 }
 interface IProps { }
 
@@ -24,7 +57,8 @@ export default class Coffee extends React.PureComponent<IProps, IState> {
         this.state = {
             categoryId: '',
             categoryDescription: '',
-            pictureList: []
+            pictureList: [],
+            products: [],
         }
     }
 
@@ -39,8 +73,19 @@ export default class Coffee extends React.PureComponent<IProps, IState> {
                     pictureList: categoryInfo.pic_list.map((pic: String) =>
                         process.env.REACT_APP_BACKEND + '/images/' + pic)
                 });
-                console.log(this.state)
-            });
+                //console.log(this.state)
+
+                Axios.get(`${process.env.REACT_APP_BACKEND}/product`)
+                    .then(res => {
+                        let productList = res.data.filter((product: IProduct) => product.ref_category === this.state.categoryId);
+
+                        this.setState({ products: productList })
+
+                    })
+
+            })
+
+
     }
 
 
@@ -55,6 +100,30 @@ export default class Coffee extends React.PureComponent<IProps, IState> {
                     <div className="images image2"><img src={this.state.pictureList[1]} alt="pic" /></div>
                     <div className="images image3"><img src={this.state.pictureList[2]} alt="pic" /></div>
                     <div className="images image4"><img src={this.state.pictureList[3]} alt="pic" /></div>
+                </div>
+
+                <div className="product-container">
+
+                    <Row type="flex" justify="center">
+
+                        {this.state.products.map((product: IProduct) => {
+                            return (
+                                <div>
+                                    <Card hoverable size="default" style={cardStyle} bodyStyle={bodyStyle}>
+                                        <div className="pics">
+                                            <img src={process.env.REACT_APP_BACKEND + '/images/' + product.pic_list[0]} alt={product.title} />
+                                        </div>
+                                        <br />
+                                        <div className="title">{product.title}<br />
+                                            {product.price}<br />
+                                            {product.manufacturer}
+                                        </div>
+                                    </Card>
+                                </div>)
+                        })
+                        }
+
+                    </Row>
                 </div>
 
             </div>
